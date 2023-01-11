818dc14971885e7dcb63c1a740f4468d93acc1ab
Alejandro Damas <alejandrodamas3d@gmail.com>
2023-01-11 14:30:21 +0100

    E04 fetch pokemons

--- e04_poke_list/lib/main.dart
+++ e04_poke_list/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -22,3 +24,64 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Future<Pokemon> fetchPoke() async {
+  final response = await http
+      .get(Uri.parse('https://pokeapi.co/api/v2/pokemon?limit=10&#39'));
+
+  if (response.statusCode == 200) {
+    return Pokemon.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load pokemon');
+  }
+}
+
+class Pokemon {
+  int? count;
+  String? next;
+  Null? previous;
+  List<Results>? results;
+
+  Pokemon({this.count, this.next, this.previous, this.results});
+
+  Pokemon.fromJson(Map<String, dynamic> json) {
+    count = json['count'];
+    next = json['next'];
+    previous = json['previous'];
+    if (json['results'] != null) {
+      results = <Results>[];
+      json['results'].forEach((v) {
+        results!.add(new Results.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['count'] = this.count;
+    data['next'] = this.next;
+    data['previous'] = this.previous;
+    if (this.results != null) {
+      data['results'] = this.results!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Results {
+  String? name;
+  String? url;
+
+  Results({this.name, this.url});
+
+  Results.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['url'] = this.url;
+    return data;
+  }
+}
