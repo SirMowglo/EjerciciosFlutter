92c0c4090d8309da9e471636f0235542a2f98047
Alejandro Damas <alejandrodamas3d@gmail.com>
2023-01-17 13:45:15 +0100

    Lista pokemons terminada

--- e04_poke_list/lib/main.dart
+++ e04_poke_list/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:basic_utils/basic_utils.dart';
 import 'package:e04_poke_list/poke_details.dart';
 import 'package:e04_poke_list/poke_response.dart';
 import 'package:flutter/material.dart';
@@ -102,6 +103,7 @@ Future<PokeDetails> fetchDetails(url) async {
 
   if (response.statusCode == 200) {
     details = PokeDetails.fromJson(jsonDecode(response.body));
+
     return details;
   } else {
     throw Exception('Failed to load Details');
@@ -127,18 +129,71 @@ class _DetailPokeState extends State<DetailPoke> {
 
   @override
   Widget build(BuildContext context) {
-    const title = "Los pokemone";
     return Scaffold(
         appBar: AppBar(
-          title: const Text(title),
+          title: const Text("Los Pokemone"),
         ),
         body: Padding(
-          padding: EdgeInsets.all(15.0),
+          padding: const EdgeInsets.only(right: 10, left: 10.0, bottom: 10.0),
           child: FutureBuilder<PokeDetails>(
               future: details,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Text(snapshot.data!.types!.first!.type!.name!);
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 255, 241, 196),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: Column(children: [
+                      Text(
+                        StringUtils.capitalize(snapshot.data!.name!),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 35,
+                        ),
+                      ),
+                      Text(
+                        '#${snapshot.data!.id}',
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w800),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image(
+                            image: NetworkImage(
+                                snapshot.data!.sprites!.frontDefault!),
+                          ),
+                          Image(
+                            image: NetworkImage(
+                                snapshot.data!.sprites!.frontShiny!),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Tipos: ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          if (snapshot.data!.types.isNotEmpty)
+                            Text(StringUtils.capitalize(
+                                snapshot.data!.types[0].type!.name!)),
+                          if (snapshot.data!.types.length > 1)
+                            const SizedBox(width: 10),
+                          if (snapshot.data!.types.length > 1)
+                            Text(StringUtils.capitalize(
+                                snapshot.data!.types[1].type!.name!)),
+                        ],
+                      ),
+                    ]),
+                  );
                 } else if (snapshot.hasError) {
                   return Text('${snapshot.error}');
                 }
